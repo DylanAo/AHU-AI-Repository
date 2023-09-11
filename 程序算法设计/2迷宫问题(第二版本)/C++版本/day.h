e52d06189a8b93f75c218ed30d7109384aa20091
DylanAo <102361886+DylanAo@users.noreply.github.com>
2023-09-11 08:55:12 +0800

    Delete 程序算法设计/2迷宫问题(第二版本) directory

--- 程序算法设计/2迷宫问题(第二版本)/C++版本/day.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "bank.h"
-#include<iostream>
-
-using namespace std;
-
-void day_run(Bank& bank, Customers& cs);
-void week_run(realtime work, realtime off, realtime sar_work, realtime sar_off, realtime begin, realtime end, bool rest, bool duty);
